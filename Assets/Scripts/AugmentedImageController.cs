bd58f6da71e35d65ec1cd401a22446fe9512eb82
giaabaoo <dhgbao@apcs.vn>
2020-09-20 13:21:46 +0700

    1st commit

---
+++ Assets/Scripts/AugmentedImageController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+
+public class AugmentedImageController : MonoBehaviour
+{
+    [SerializeField] private AugmentedImageVisualizer augmented_image_visualizer;
+
+    private readonly Dictionary<int, AugmentedImageVisualizer> visualizers = 
+        new Dictionary<int, AugmentedImageVisualizer>();
+
+    private readonly List<AugmentedImage> images = new List<AugmentedImage>();
+
+    private void Update() {
+        if (Session.Status != SessionStatus.Tracking) {
+            return;
+        }
+
+        Session.GetTrackables(images, TrackableQueryFilter.Updated);
+        VisualizeTrackables();
+    }
+
+    private void VisualizeTrackables() {
+        foreach (var image in images) {
+            var visualizer = GetVisualizer(image);
+
+            if (image.TrackingState == TrackingState.Tracking && visualizer == null) {
+                AddVisualizer(image);
+            }
+
+            else if (image.TrackingState == TrackingState.Stopped && visualizer != null) {
+                RemoveVisualizer(image, visualizer);
+            }
+        }
+    }
+
+    private void RemoveVisualizer(AugmentedImage image, AugmentedImageVisualizer visualizer) {
+        visualizers.Remove(image.DatabaseIndex);
+        Destroy(visualizer.gameObject);
+    }
+    
+    private void AddVisualizer(AugmentedImage image) {
+        var anchor = image.CreateAnchor(image.CenterPose);
+        var visualizer = Instantiate(augmented_image_visualizer, anchor.transform);
+        visualizer.image = image;
+        visualizers.Add(image.DatabaseIndex, visualizer);
+    }
+
+    private AugmentedImageVisualizer GetVisualizer(AugmentedImage image) {
+        AugmentedImageVisualizer visualizer;
+        visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+        return visualizer;
+    }
+}
---
+++ Assets/Scripts/AugmentedImageVisualizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using GoogleARCore;
+
+public class AugmentedImageVisualizer : MonoBehaviour
+{
+    [SerializeField] private VideoClip[] videoClips;
+    public AugmentedImage image;
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += OnStop;
+    }
+
+    private void OnStop(VideoPlayer source) {
+        gameObject.SetActive(false);
+    }
+
+    void Update() {
+        if (image == null | image.TrackingState != TrackingState.Tracking) {
+            return;
+        }
+
+        if (!videoPlayer.isPlaying) {
+            videoPlayer. clip = videoClips[image.DatabaseIndex];
+            videoPlayer.Play();
+        }
+
+        transform.localScale = new Vector3(image.ExtentX, image.ExtentZ, 1);
+
+    }
+
+}
